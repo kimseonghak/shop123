dc899a192a67b11787ad323f203728030e64176a
82109 <82109@LAPTOP-2NHAJSSD>
2022-01-31 22:51:08 +0900

    1.리스트, 글 작성 페이지로 연결 구현(일단 header에서 가는건 링크 뺴놓은 상태

--- src/main/java/com/hot/shop/question/model/dao/QuestionDAO.java
+++ src/main/java/com/hot/shop/question/model/dao/QuestionDAO.java
@@ -1,5 +1,23 @@
 package com.hot.shop.question.model.dao;
 
+import java.util.ArrayList;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+import com.hot.shop.question.model.vo.QuestionUser;
+
+@Repository
 public class QuestionDAO {
 
+	@Autowired
+	@Qualifier(value="sqlSessionTemplate")
+	private SqlSessionTemplate sqlSession;
+
+	public ArrayList<QuestionUser> selectUserQuestionList() {
+		
+		return new ArrayList<QuestionUser>(sqlSession.selectList("qUser.selectUserQuestionList"));
+	}
 }
--- src/main/java/com/hot/shop/question/model/service/QuestionServiceImpl.java
+++ src/main/java/com/hot/shop/question/model/service/QuestionServiceImpl.java
@@ -1,5 +1,30 @@
 package com.hot.shop.question.model.service;
 
-public class QuestionServiceImpl {
+import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hot.shop.question.model.dao.QuestionDAO;
+import com.hot.shop.question.model.vo.QuestionUser;
+
+@Service
+public class QuestionServiceImpl implements QuestionService{
+
+	@Autowired
+	private QuestionDAO qDAO;
+
+	@Override
+	public ArrayList<QuestionUser> selectUserQuestionList() {
+		return qDAO.selectUserQuestionList();
+	}
+	
+	@Override
+	public QuestionUser QuestionUserWrite() {
+		return null;
+	}
+
+
+
+	
 }
